6128e7dda634309f1c43a8993a620c900ed2a0bb
Rangan Pratik Borah <ranganpratikborah2006@gmail.com>
2026-04-18 17:14:52 +0530

    fixed redirect url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
+const isLocal =
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname === "localhost";
+
 export const spotifyConfig = {
   clientId: "35b6278bab4841a8b48391e099e79e1f",
-  redirectUri: "http://127.0.0.1:5173/callback",
+  redirectUri: isLocal
+    ? "http://127.0.0.1:5173/"
+    : "https://moodora-ai.vercel.app/",
   scopes: [
     "streaming",
     "user-read-email",
